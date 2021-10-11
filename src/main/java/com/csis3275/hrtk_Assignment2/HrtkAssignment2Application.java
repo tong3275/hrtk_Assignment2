0545fdf3469ea990bb51172777127743e6ba2be2
Rodrigo Freitas <rodrigo@syligo.com>
2021-10-11 11:58:33 -0700

    [bootstrap] creating the initial project structure; Closes #1

---
+++ src/main/java/com/csis3275/hrtk_Assignment2/HrtkAssignment2Application.java
@@ -0,0 +1,13 @@
+package com.csis3275.hrtk_Assignment2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrtkAssignment2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrtkAssignment2Application.class, args);
+	}
+
+}
